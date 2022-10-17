34ec8607ef394364a7df679d08b1ea389c462dd2
Periyardhasan D <113511673+DoomSlicer-peri@users.noreply.github.com>
2022-10-17 23:56:31 +0530

    Add files via upload

---
+++ Primitive DataType-Integer/src/DataTypeCasting/DataTypeCasting.java
@@ -0,0 +1,16 @@
+package DataTypeCasting;
+
+public class DataTypeCasting {
+
+	public static void main(String[] args) {
+		float f=10.532f;
+		long l=(long)f;
+		double d=f;
+		byte b=12;
+		short s=b;
+		int a=1;
+		System.out.println(l);
+
+	}
+
+}
